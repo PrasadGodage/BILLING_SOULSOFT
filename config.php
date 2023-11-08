8be4abdb9e8a8b857a395bd7c1137c24517d89ff
Rahuldeshmukhsoulsoft <148740675+Rahuldeshmukhsoulsoft@users.noreply.github.com>
2023-11-08 09:43:12 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 
 <?php
 // 
- $con=mysqli_connect("localhost","root","","curvedent")or die(mysqli_error($con));
-//  $con=mysqli_connect("localhost","u311423116_sparksoul","cY!V*:M3","u311423116_sparkbilling")or die(mysqli_error($con));
+//  $con=mysqli_connect("localhost","root","","curvedent")or die(mysqli_error($con));
+ $con=mysqli_connect("localhost","u311423116_soulsoft","2[cHxWlW]","u311423116_soulsoftBill")or die(mysqli_error($con));
 
 
 
